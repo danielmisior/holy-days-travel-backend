5c8dfa1b022412826e8084d46a82f3d9e905be53
Daniel Misior <daniel.misior09@gmail.com>
2022-09-01 10:57:56 +0200

    Initial commit

---
+++ src/test/java/com/backend/holydaystravel/HolyDaysTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.holydaystravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyDaysTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
